7e9adb00d9267b4db700fdda881814ab42dce68d
GoggggF <home09040000@gmail.com>
2023-09-20 15:48:57 +0700

    Занятие-2-Практика

---
+++ Практика.js
@@ -0,0 +1,24 @@
+Задание 1
+let d = "Привет"
+console.log(d)
+Задание 2
+let length = 5
+let height = 2
+let square = length*height/2
+console.log(square)
+Задание 3
+я не понял
+Задание 4
+let number = 5
+if (number > 0) {
+    console.log("Число положительное")
+} else if (number < 0){
+    console.log("Число отрицательное")
+} else if (numder == 0){
+    console.log("Число равно нулю")
+}
+Задание 5
+интересная, но немного не понял да и мне надо идти к репетитору "((("
+
+
+
